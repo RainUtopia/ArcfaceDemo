234dd35704ece2274cad24bce939249add19b3c2
931663592 <zcoolshuai18O5>
2019-04-07 10:23:38 +0800

    Initial commit

---
+++ app/src/main/java/com/arcsoft/arcfacedemo/common/Constants.java
@@ -0,0 +1,6 @@
+package com.arcsoft.arcfacedemo.common;
+
+public class Constants {
+    public static final String APP_ID = "CwQdSv9WYeHLnxFqoW9RHYX1Y9q7e35SX632dftsGCNW";
+    public static final String SDK_KEY = "4dYQxEvw6QekktSVX3s4VU1wAgo2VewsHaWyZcNi897q";
+}
